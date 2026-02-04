b3dd0d337455f40cb8ba9d07f9233bf742397b2a
melly-mell <slingala0812@gmail.com>
2026-02-03 19:19:49 -0500

    requests df moved into for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -25,6 +25,10 @@ if ingredients_list:
      ingredients_string = ''
      for fruit_chosen in ingredients_list:
          ingredients_string += fruit_chosen + ' '
+         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+         #st.text(smoothiefroot_response.json())
+         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
      #st.write(ingredients_string)
      
      my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -36,8 +40,5 @@ if ingredients_list:
      if time_to_insert:
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered!', icon="✅")
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
